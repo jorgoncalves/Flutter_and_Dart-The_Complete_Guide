a9fc10b33845ab6efb46c84666ddb28641a00e3a
Jorge Gonçalves <jorge.oliveira.goncalves@outlook.pt>
2021-04-03 13:42:03 +0100

    .

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,64 @@
+import 'package:app/screens/edit_product_screen.dart';
+
+import '../widgets/app_drawer.dart';
+
+import 'package:app/widgets/user_product_item.dart';
+
+import '../providers/products.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your products'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                try {
+                  Navigator.of(context).pushNamed(EditProductScreen.routeName);
+                } catch (error) {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Deleting failed!"),
+                    ),
+                  );
+                }
+              }),
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (_, i) => Column(
+              children: [
+                UserProductItem(
+                  id: productsData.items[i].id,
+                  title: productsData.items[i].title,
+                  imageUrl: productsData.items[i].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
+          ),
+        ),
+      ),
+      drawer: AppDrawer(),
+    );
+  }
+}
